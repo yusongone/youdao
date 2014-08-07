71e88b2282dc601c1c00c34bc3d1126f93dd65ae
root <root@localhost.localdomain>
2014-08-07 03:02:52 +0000

    file

--- router/lib/ajax_router.js
+++ router/lib/ajax_router.js
@@ -1,7 +1,10 @@
 var youdaoModel=require("../../model").youdao;
+var count=0;
 function init(app){
     app.post("/getTranslateData",function(req,res){
         var requestString=req.body.q;
+		count++;
+		console.log(count+":== "+requestString + "");
        youdaoModel.getData(requestString,function(err,string){
            if(err){
             }
